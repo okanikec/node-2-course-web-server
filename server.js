acb2acbdc334934414b9d97dd32da8c65b43eb1d
okanikec <okanikec@isu.edu>
2017-08-12 11:53:01 -0500

    Add project page

--- server.js
+++ server.js
@@ -69,7 +69,12 @@ app.get('/about',(req,res) => {
 		});
 });
 
-
+//
+app.get('/projects',(req,res) => {
+	res.render('projects.hbs',{
+					pageTitle: 'Projects'
+					});
+});
 
 
 //bad request - send back json with error message
